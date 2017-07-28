2c00c259adc522c24487c36318c28ceac6970c3a
孙博为 <bowei.sun@acorn-net.com>
2017-07-28 11:09:37 +0800

    版本1.0

--- server/controller/musicController.js
+++ server/controller/musicController.js
@@ -9,24 +9,26 @@ var fs = require("fs"),
 module.exports = function(app){
     var getFileContent = function(path){
         path = "./public/files/"+path;
-        fs.open(path, 'r', function(err, fd) {
-            if (err) {
-                throw err;
-            }
-            console.log('open file success.');
-            var buffer = new Buffer(255);
-            // 读取文件
-            fs.read(fd, buffer, 0, 10, 0, function(err, bytesRead, buffer) {
-                if (err) {
-                    throw err;
-                }
-                // 打印出buffer中存入的数据
-                console.log(bytesRead, buffer.slice(0, bytesRead).toString());
+        var content = fs.readFileSync(path,"utf-8");
+        console.log(content);
+        // fs.open(path, 'r', function(err, fd) {
+        //     if (err) {
+        //         throw err;
+        //     }
+        //     console.log('open file success.');
+        //     var buffer = new Buffer(255);
+        //     // 读取文件
+        //     fs.read(fd, buffer, 0, 10, 0, function(err, bytesRead, buffer) {
+        //         if (err) {
+        //             throw err;
+        //         }
+        //         // 打印出buffer中存入的数据
+        //         console.log(bytesRead, buffer.slice(0, bytesRead).toString());
 
-                // 关闭文件
-                fs.close(fd);
-            });
-        });
+        //         // 关闭文件
+        //         fs.close(fd);
+        //     });
+        // });
     };
 
     //查找音乐列表
