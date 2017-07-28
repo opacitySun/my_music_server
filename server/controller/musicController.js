82c9d8ad7c00416792efb914c021658c534b2f88
孙博为 <bowei.sun@acorn-net.com>
2017-07-28 11:12:47 +0800

    版本1.0

--- server/controller/musicController.js
+++ server/controller/musicController.js
@@ -10,25 +10,7 @@ module.exports = function(app){
     var getFileContent = function(path){
         path = "./public/files/"+path;
         var content = fs.readFileSync(path,"utf-8");
-        console.log(content);
-        // fs.open(path, 'r', function(err, fd) {
-        //     if (err) {
-        //         throw err;
-        //     }
-        //     console.log('open file success.');
-        //     var buffer = new Buffer(255);
-        //     // 读取文件
-        //     fs.read(fd, buffer, 0, 10, 0, function(err, bytesRead, buffer) {
-        //         if (err) {
-        //             throw err;
-        //         }
-        //         // 打印出buffer中存入的数据
-        //         console.log(bytesRead, buffer.slice(0, bytesRead).toString());
-
-        //         // 关闭文件
-        //         fs.close(fd);
-        //     });
-        // });
+        return content;
     };
 
     //查找音乐列表
@@ -42,7 +24,7 @@ module.exports = function(app){
         dbHelper.findData('music',column,where,fields,function(result){
             if(req.query.id){
                 result.result.forEach(function(obj){
-                    getFileContent(obj.lyric);
+                    obj.lyric = getFileContent(obj.lyric);
                 });
             }
             if(_callback){
