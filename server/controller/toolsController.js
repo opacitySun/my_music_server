fd59746e6dae02c41bb3d35d108e2525889f81c3
孙博为 <bowei.sun@acorn-net.com>
2017-08-01 15:22:36 +0800

    版本1.0

--- server/controller/toolsController.js
+++ server/controller/toolsController.js
@@ -1,5 +1,10 @@
 var Dayu = require('alidayu-node');
 var DayuNew = new Dayu('LTAIATC5U00WQH0q','9LOMao1HSd3nzxYqXuavZKLeDeFgE7');
+var SMS = require('aliyun-sms-node');
+var sms = new SMS({
+	AccessKeyId: 'LTAIATC5U00WQH0q',
+	AccessKeySecret: '9LOMao1HSd3nzxYqXuavZKLeDeFgE7'
+});
 
 var GetRandomNum = function(n){
 	var chars = ['0','1','2','3','4','5','6','7','8','9'];
@@ -27,24 +32,34 @@ module.exports = function(app){
     	var _callback = req.query.callback,
     		mobile = req.query.mobile;
     	var number = GetRandomNum(6);
-		DayuNew.smsSend({
-		    // sms_free_sign_name: '孙博为', //短信签名，参考这里 http://www.alidayu.com/admin/service/sign
-		    // sms_param: JSON.stringify({"number": number}),//短信变量，对应短信模板里面的变量
-		    // rec_num: mobile, //接收短信的手机号
-		    // sms_template_code: 'SMS_80110091' //短信模板，参考这里 http://www.alidayu.com/admin/service/tpl
-		    sms_free_sign_name: '注册验证', //短信签名，参考这里 http://www.alidayu.com/admin/service/sign
-		    sms_param: JSON.stringify({"code": "123456", "product": "测试网站"}),//短信变量，对应短信模板里面的变量
-		    rec_num: '13466348313', //接收短信的手机号
-		    sms_template_code: 'SMS_640004' //短信模板，参考这里 http://www.alidayu.com/admin/service/tpl
-		},function(error,response){
-			var result;
-			if(error){
-				result = error;
-			}else{
-				result = response;
-			}
+		// DayuNew.smsSend({
+		//     sms_free_sign_name: '孙博为', //短信签名，参考这里 http://www.alidayu.com/admin/service/sign
+		//     sms_param: JSON.stringify({"number": number}),//短信变量，对应短信模板里面的变量
+		//     rec_num: mobile, //接收短信的手机号
+		//     sms_template_code: 'SMS_80110091' //短信模板，参考这里 http://www.alidayu.com/admin/service/tpl
+		// },function(error,response){
+		// 	var result;
+		// 	if(error){
+		// 		result = error;
+		// 	}else{
+		// 		result = response;
+		// 	}
+		// 	res.type('text/javascript');
+  //           res.send(_callback + '(' + JSON.stringify(result) + ')');
+		// });
+    	sms.send({
+			Format: 'JSON',
+			Action: 'SendSms',
+			TemplateParam: '{"number":'+number+'}',
+			PhoneNumbers: mobile,
+			SignName: '孙博为',
+			TemplateCode: 'SMS_80110091'
+		}).then(function(result){
 			res.type('text/javascript');
-            res.send(_callback + '(' + JSON.stringify(result) + ')');
+			res.send(_callback + '(' + JSON.stringify(result) + ')');
+		}).catch(function(error){
+			res.type('text/javascript');
+			res.send(_callback + '(' + JSON.stringify(error) + ')');
 		});
     });
 
