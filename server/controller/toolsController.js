4c8c2231995ada820370db875276bb19b0472ce8
孙博为 <bowei.sun@acorn-net.com>
2017-08-01 09:15:02 +0800

    版本1.0

--- server/controller/toolsController.js
+++ server/controller/toolsController.js
@@ -5,7 +5,7 @@ var GetRandomNum = function(n){
 	var chars = ['0','1','2','3','4','5','6','7','8','9'];
 	var res = "";
 	for(var i = 0; i < n ; i ++) {
-		var id = Math.ceil(Math.random()*35);
+		var id = Math.ceil(Math.random()*9);
 		res += chars[id];
 	}
 	return res;
