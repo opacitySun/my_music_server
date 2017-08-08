e004e2148104d19778c1ef595ab965db825e6cd2
孙博为 <bowei.sun@acorn-net.com>
2017-08-08 17:57:38 +0800

    版本1.0

--- server/DBHelper/redis.js
+++ server/DBHelper/redis.js
@@ -1,11 +1,28 @@
 var redis = require('redis'),
 	RDS_PORT = 6379,        //端口号
     RDS_HOST = '127.0.0.1',    //服务器IP
-    RDS_OPTS = {},            //设置项
+    RDS_PWD = 'xJmcMhmgGoYlcA69',
+    RDS_OPTS = {auth_pass:RDS_PWD},   //设置项
     client = redis.createClient(RDS_PORT,RDS_HOST,RDS_OPTS);
 
 // redis 链接错误
-client.on("error", function(error) {
-    console.log(error);
+var isError = function(){
+	client.on("error", function(error) {
+	    if(error){
+	    	console.log(error);
+	    	return true;
+	    }else{
+	    	return false;
+	    }
+	});
+};
+
+// redis 链接成功
+client.on("ready", function(res) {
+    console.log('success');
 });
+
+module.exports = {
+	
+};
 
