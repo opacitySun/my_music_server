43b0d651ca22b09c829ae5db0fd21a74cd8b33b6
opacitySun <809340810@qq.com>
2019-03-15 14:44:14 +0800

    v1.0

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ var express = require('express'),
     ejs = require('ejs'),
     expressSession = require('express-session'),
     redisStore = require('connect-redis')(expressSession),
-    partials = require('express-partials'),
-    nodexcn = require('nodexcn'),
-    ueditor = require("ueditor");
+    partials = require('express-partials');
 
 var routes = require('./routes/index'),
     users = require('./routes/users');
@@ -38,31 +36,6 @@ app.use(expressSession({
   secret: 'sunMusic '+toolsController.GetRandomString(10)
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-//操作ueditor的图片上传功能
-app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
-  // ueditor 客户发起上传图片请求
-  if(req.query.action === 'uploadimage'){
-    // 这里你可以获得上传图片的信息
-    var foo = req.ueditor;
-    console.log(foo.filename); // exp.png
-    console.log(foo.encoding); // 7bit
-    console.log(foo.mimetype); // image/png
-    // 下面填写你要把图片保存到的路径 （ 以 path.join(__dirname, 'public') 作为根路径）
-    var img_url = '/files/';
-    res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
-  }
-  //客户端发起图片列表请求
-  else if (req.query.action === 'listimage'){
-    var dir_url = '/files/'; // 要展示给客户端的文件夹路径
-    res.ue_list(dir_url) // 客户端会列出 dir_url 目录下的所有图片
-  }
-  // 客户端发起其它请求
-  else {
-    res.setHeader('Content-Type', 'application/json');
-    // 这里填写 ueditor.config.json 这个文件的路径
-    res.redirect('/ueditor/ueditor.config.json');
-}}));
 
 //将express与控制器相关联来达到路由的目的
 routesController(app);
